b19a91696777022b70dba14b3fce91092b1fbb30
ThailanySantos <eng.thailanysantos@gmail.com>
2024-09-19 16:52:17 -0300

    ultimas-modificacoes

--- projeto-app-calc/src/App.jsx
+++ projeto-app-calc/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react":
 import Display from "./Componentes/Display/display";
 import ButtonPanel from "./Componentes/Button Panel/buttonPanel";
 import "../src/App.css";
